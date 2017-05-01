9a0a6a207ccc570d48b8ab428ea7a6d1bf64236c
tarnium <tjurasek@gmail.com>
2017-05-01 18:21:21 +0200

    UI

--- QuestGenerator/Form1.Designer.cs
+++ QuestGenerator/Form1.Designer.cs
@@ -28,20 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.paramPage = new System.Windows.Forms.TabPage();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.Interactive = new System.Windows.Forms.TabPage();
+            this.simpleGraph = new System.Windows.Forms.TabPage();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl.SuspendLayout();
+            this.paramPage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tabControl.Controls.Add(this.paramPage);
+            this.tabControl.Controls.Add(this.Interactive);
+            this.tabControl.Controls.Add(this.simpleGraph);
+            this.tabControl.Location = new System.Drawing.Point(1, 1);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(802, 534);
+            this.tabControl.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
+            this.tabControl.TabIndex = 0;
+            // 
+            // paramPage
+            // 
+            this.paramPage.Controls.Add(this.label5);
+            this.paramPage.Controls.Add(this.label4);
+            this.paramPage.Controls.Add(this.label3);
+            this.paramPage.Controls.Add(this.label2);
+            this.paramPage.Controls.Add(this.label1);
+            this.paramPage.Controls.Add(this.listView1);
+            this.paramPage.Controls.Add(this.numericUpDown2);
+            this.paramPage.Controls.Add(this.numericUpDown1);
+            this.paramPage.Controls.Add(this.checkedListBox2);
+            this.paramPage.Controls.Add(this.checkedListBox1);
+            this.paramPage.Location = new System.Drawing.Point(4, 29);
+            this.paramPage.Name = "paramPage";
+            this.paramPage.Padding = new System.Windows.Forms.Padding(3);
+            this.paramPage.Size = new System.Drawing.Size(794, 501);
+            this.paramPage.TabIndex = 0;
+            this.paramPage.Text = "Parameters";
+            this.paramPage.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(56, 33);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkedListBox1.Size = new System.Drawing.Size(183, 172);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.ThreeDCheckBoxes = true;
+            // 
+            // Interactive
+            // 
+            this.Interactive.Location = new System.Drawing.Point(4, 29);
+            this.Interactive.Name = "Interactive";
+            this.Interactive.Padding = new System.Windows.Forms.Padding(3);
+            this.Interactive.Size = new System.Drawing.Size(794, 405);
+            this.Interactive.TabIndex = 1;
+            this.Interactive.Text = "Interactive";
+            this.Interactive.UseVisualStyleBackColor = true;
+            // 
+            // simpleGraph
+            // 
+            this.simpleGraph.Location = new System.Drawing.Point(4, 29);
+            this.simpleGraph.Name = "simpleGraph";
+            this.simpleGraph.Padding = new System.Windows.Forms.Padding(3);
+            this.simpleGraph.Size = new System.Drawing.Size(794, 405);
+            this.simpleGraph.TabIndex = 2;
+            this.simpleGraph.Text = "Simple Graph";
+            this.simpleGraph.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(281, 33);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkedListBox2.Size = new System.Drawing.Size(183, 172);
+            this.checkedListBox2.TabIndex = 1;
+            this.checkedListBox2.ThreeDCheckBoxes = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(618, 33);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(618, 66);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(281, 276);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(183, 164);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(515, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Max Depth";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(515, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "# of Options";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Motivations";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(281, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Focus";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(281, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "People";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 532);
+            this.Controls.Add(this.tabControl);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl.ResumeLayout(false);
+            this.paramPage.ResumeLayout(false);
+            this.paramPage.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage paramPage;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TabPage Interactive;
+        private System.Windows.Forms.TabPage simpleGraph;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
